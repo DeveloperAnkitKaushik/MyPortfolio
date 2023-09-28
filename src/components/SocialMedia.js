e666cdc42bc513cfe3e1811ce9f3e8b098d2f46a
Ankit Kaushik <ankitkaushik6269@gmail.com>
2023-09-28 13:09:04 +0530

    Minor Changes

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsTwitter, BsGithub } from 'react-icons/bs';
+import { FaLinkedin, FaGithubAlt } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
 const SocialMedia = () => (
   <div className="app__social">
-    <div>
-      <BsTwitter/>
+    <div><a href="https://twitter.com/Ankit_kaushik0" target='_blank'>
+      <BsTwitter/></a>
     </div>
-    <div>
-      <FaFacebookF />
+    <div><a href="https://www.linkedin.com/in/ankitkaushik/" target='_blank'>
+      <FaLinkedin /></a>
     </div>
-    <div>
-      <BsInstagram />
+    <div><a href="https://github.com/DeveloperAnkitKaushik" target='_blank'>
+      <FaGithubAlt /></a>
     </div>
   </div>
 );
